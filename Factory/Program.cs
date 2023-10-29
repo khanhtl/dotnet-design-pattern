966874ec81e0576dd60321693ccef7d9c3129c0f
tlkhanh <longkhanhta@gmail.com>
2023-10-29 20:48:12 +0700

    Simple Factory

---
+++ Factory/Program.cs
@@ -0,0 +1,16 @@
+﻿using BumbleBikesLibrary;
+using SimpleFactory;
+
+const string errorText = "You must pass in mountainbike, cruiser, recumbent, or roadbike";
+Console.Write("Bike want to build: ");
+var bicycleType = Console.ReadLine();
+if (bicycleType.Length > 0)
+{
+    var bicycleFactory = new SimpleBicycleFactory();
+    var bikeToBuild = bicycleFactory.CreateBicycle(bicycleType);
+    bikeToBuild.Build();
+}
+else
+{
+    Console.WriteLine(errorText);
+}
---
+++ Factory/SimpleBicycleFactory.cs
@@ -0,0 +1,35 @@
+﻿using BumbleBikesLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFactory
+{
+    public class SimpleBicycleFactory
+    {
+        public Bicycle CreateBicycle(string bicycleType)
+        {
+            Bicycle bikeToBuild;
+            switch (bicycleType)
+            {
+                case "mountainbike":
+                    bikeToBuild = new MountainBike();
+                    break;
+                case "cruiser":
+                    bikeToBuild = new Cruiser();
+                    break;
+                case "recumbent":
+                    bikeToBuild = new Recumbent();
+                    break;
+                case "roadbike":
+                    bikeToBuild = new RoadBike();
+                    break;
+                default:
+                    throw new Exception("Unknown bicycle type: " + bicycleType);
+            }
+            return bikeToBuild;
+        }
+    }
+}
