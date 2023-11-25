a46fd9e2b99be713edc06243348e9d14702d2b0c
tlkhanh <longkhanhta@gmail.com>
2023-11-25 15:06:33 +0700

    string builder

---
+++ Builder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace Builder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "hello";
+            var sb = new StringBuilder();
+            sb.Append("<p>");
+            sb.Append(hello);
+            sb.AppendLine("</p>");
+            Console.WriteLine(sb);
+
+            var words = new[] { "hello", "world" };
+            sb.Clear();
+            sb.Append("<ul>");
+            foreach (var word in words)
+            {
+                sb.AppendFormat("<li>{0}</li>", word);
+            }
+            sb.AppendLine("</ul>");
+            Console.WriteLine(sb);
+        }
+    }
+}
