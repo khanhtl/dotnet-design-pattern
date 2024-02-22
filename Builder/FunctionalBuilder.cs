a942c04d424286900998cf8b41000a2a0f4d427e
tlkhanh <longkhanhta@gmail.com>
2024-02-22 19:13:55 +0700

    Functional Builde

---
+++ Builder/FunctionalBuilder.cs
@@ -0,0 +1,73 @@
+﻿namespace Builder.FunctionalBuilder
+{
+    public class Person
+    {
+        public string Name, Position;
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+
+    }
+
+    public abstract class FunctionalBuilder<TSubject, TSelf>
+        where TSelf : FunctionalBuilder<TSubject, TSelf>
+        where TSubject : new()
+    {
+        private readonly List<Func<TSubject, TSubject>> actions = new List<Func<TSubject, TSubject>>();
+
+        public TSubject Build() => actions.Aggregate(new TSubject(), (p, f) => f(p));
+        public TSelf Do(Action<TSubject> action)
+        {
+            AddAction(action);
+            return (TSelf)this;
+        }
+        private TSelf AddAction(Action<TSubject> action)
+        {
+            actions.Add(p =>
+            {
+                action(p);
+                return p;
+            });
+            return (TSelf)this;
+        }
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorkAsA(this PersonBuilder builder, string position)
+            => builder.Do(p => p.Position = position);
+    }
+    public static class PersonBuilderExtensions2
+    {
+        public static PersonBuilder2 WorkAsA(this PersonBuilder2 builder, string position)
+            => builder.Do(p => p.Position = position);
+    }
+    public sealed class PersonBuilder
+    {
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+        public PersonBuilder Called(string name) => Do(p => p.Name = name);
+
+        public Person Build() => actions.Aggregate(new Person(), (p, f) => f(p));
+        public PersonBuilder Do(Action<Person> action)
+        {
+            AddAction(action);
+            return this;
+        }
+        private PersonBuilder AddAction(Action<Person> action)
+        {
+            actions.Add(p =>
+            {
+                action(p);
+                return p;
+            });
+            return this;
+        }
+
+    }
+
+    public sealed class PersonBuilder2 : FunctionalBuilder<Person, PersonBuilder2>
+    {
+        public PersonBuilder2 Called(string name) => Do(p => p.Name = name);
+    }
+}
--- Builder/Program.cs
+++ Builder/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+﻿using Builder.FunctionalBuilder;
 
 namespace Builder
 {
@@ -6,31 +6,34 @@ namespace Builder
     {
         static void Main(string[] args)
         {
-            var hello = "hello";
-            var sb = new StringBuilder();
-            sb.Append("<p>");
-            sb.Append(hello);
-            sb.AppendLine("</p>");
-            Console.WriteLine(sb);
+            //var hello = "hello";
+            //var sb = new StringBuilder();
+            //sb.Append("<p>");
+            //sb.Append(hello);
+            //sb.AppendLine("</p>");
+            //Console.WriteLine(sb);
 
-            var words = new[] { "hello", "world" };
-            sb.Clear();
-            sb.Append("<ul>");
-            foreach (var word in words)
-            {
-                sb.AppendFormat("<li>{0}</li>", word);
-            }
-            sb.AppendLine("</ul>");
-            Console.WriteLine(sb);
+            //var words = new[] { "hello", "world" };
+            //sb.Clear();
+            //sb.Append("<ul>");
+            //foreach (var word in words)
+            //{
+            //    sb.AppendFormat("<li>{0}</li>", word);
+            //}
+            //sb.AppendLine("</ul>");
+            //Console.WriteLine(sb);
 
-            var builder = new HtmlBuilder("ul");
-            builder
-                .AddChild("li", "hello")
-                .AddChild("li", "world");
-            Console.WriteLine(builder);
+            //var builder = new HtmlBuilder("ul");
+            //builder
+            //    .AddChild("li", "hello")
+            //    .AddChild("li", "world");
+            //Console.WriteLine(builder);
 
-            var person = Person.New.Called("Khanh").Called("SWE").Build();
+            //var person = Person.New.Called("Khanh").Called("SWE").Build();
+            var person = new Builder.FunctionalBuilder.PersonBuilder().Called("Khanh").WorkAsA("SWE").Build();
             Console.WriteLine(person);
+            var person2 = new Builder.FunctionalBuilder.PersonBuilder2().Called("Hue").WorkAsA("QC").Build();
+            Console.WriteLine(person2);
         }
     }
 }
